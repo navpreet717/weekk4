6d00bdf74f490b075c4f02980329cb4d50336d9e
748674 <748674@123-PC17.student.Tor.Loc>
2019-05-28 15:11:15 -0400

    second commit

--- prctce/src/prctce/exercise.java
+++ prctce/src/prctce/exercise.java
@@ -4,6 +4,10 @@ public class exercise {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//first program in string
+		//create a string
+		//number of characters in the string
+		//
 
 	}
 
